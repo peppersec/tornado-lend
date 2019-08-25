e7ce756136e8fe8d251357b77a3a2b49aabab906
poma <Semenov.Roman@mail.ru>
2019-08-25 02:37:03 +0200

    remove owner

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tornado lend
+
+Make your locked funds work for you!
+
+# Inspiration
+
+When we talked with people about our previous Mixer project, the first thing many people said was "Hey, you should put those locked funds to work with DeFi!", and that's what we decided to do at this hackathon.
+
+# What we did
+
+There are many smart-contracts like DAOs, Bridges, Mixers that keep funds locked in for prolonged periods of time. [Project name] allows contract creators to make those funds work by lending them to DeFi services like Compound. Since often the interest cannot be distributed directly to contract users, we use staking pool to distribute those funds. The interest pool can be also used to refund gas for users transactions. 
--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -4,18 +4,11 @@ import './CEtherLendingLibraryBase.sol';
 import './interfaces/ICEther.sol';
 
 contract Wallet is CEtherLendingLibraryBase {
-  address payable public owner;
   address payable public pool;
   uint public denomination;
   mapping(address => bool) public deposits;
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "only owner method");
-    _;
-  }
-
   constructor (ICEther _cEther, uint _denomination, address payable _pool) CEtherLendingLibraryBase(_cEther) public {
-    owner = msg.sender;
     pool = _pool;
     denomination = _denomination;
   }
@@ -36,7 +29,7 @@ contract Wallet is CEtherLendingLibraryBase {
     msg.sender.transfer(denomination);
   }
 
-  function withdrawInterest() public onlyOwner {
+  function withdrawInterest() public {
     uint interest = _withdrawInterest();
     pool.transfer(interest);
   }
