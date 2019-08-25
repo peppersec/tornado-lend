99353c0e386dae9a23aa18df9a6064dd3ef5f6c0
Alexey <peretsmobil@gmail.com>
2019-08-25 10:50:40 +0300

    the graph

---
+++ graph/src/mapping.ts
@@ -0,0 +1,25 @@
+import {
+  Transfer as TransferEvent,
+  Approval as ApprovalEvent
+} from "../generated/Contract/Contract"
+import { Transfer, Approval } from "../generated/schema"
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+  entity.save()
+}
